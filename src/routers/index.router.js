b8d8f153351be0df8ff432d12fb2e44cc5d3d294
luismanueldiaz29 <luismaprograma@gmail.com>
2020-12-23 15:05:10 -0500

    server upload files

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const siteRouter = require('./site.router.js')
+const uploadRouter = require('./upload.router');
 
 router.get('/', (req, res) => {
-    res.json({'mess': 'Index'});
+    res.send(`
+    <br><br>
+    <center>
+        <h1>Wellcome to WebGuiaCesar</h1>
+            <br><br>
+        <b style="font-size: 182px;">😃👻</b>
+        <br><br>
+        <h3>Redirect to /api/*</h3>
+    </center>
+    `);
 })
 
 module.exports = (app) => {
-    app.use('/api', router);
+    app.use('/', router);
     app.use('/api/site', siteRouter);
+    app.use('/api/upload', uploadRouter);
 }
