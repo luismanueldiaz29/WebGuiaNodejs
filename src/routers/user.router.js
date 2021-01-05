3acac16c15f27dae6c47c0ce6549a15b59972180
luismanueldiaz29 <luismaprograma@gmail.com>
2021-01-05 12:07:42 -0500

    jwt an role

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/user.controller')
 const checkJwt = require('../middlewares/jwt');
+const {checkRole} = require('../middlewares/role');
 
-router.get('/', [checkJwt], controller.get);
-router.get('/:id', [checkJwt], controller.getId);
-router.post('/', controller.post);
-router.put('/:id', controller.update)
-router.delete('/:id', controller.delete)
+router.get('/', [checkJwt, checkRole(['admin'])],controller.get);
+router.get('/:id', controller.getId);
+router.post('/', [checkJwt], controller.post);
+router.put('/:id', [checkJwt],controller.update)
+router.delete('/:id', [checkJwt], controller.delete)
 
 module.exports = router;
